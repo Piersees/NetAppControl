565e1bcf24c94791d964b6d35dd311281ee61f0f
David Deray <deray@edu.ece.fr>
2018-02-03 13:31:24 +0100

    bugfix

---
+++ ui/VPNstatusWidget.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'speedtest.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from appPopUpSecurity import appPopUpSecurity
+from appPopUpNetwork import appPopUpNetwork
+import sys
+sys.path.append("../Network")
+import SpeedTest
+import threading
+
+class VPNstatusWidget(QWidget):
+
+    def __init__(self, parent=None):
+
+        super(VPNstatusWidget, self).__init__(parent)
+
+        self.mainlayout = QHBoxLayout()
+        self.setLayout(self.mainlayout)
+
+        self.label = QLabel("VPN status:")
+        self.mainlayout.addWidget(self.label)
+
+        self.imgGreen = QPixmap('./images/vpngreen.png')
+        self.imgRed = QPixmap('./images/vpnred.png')
+
+        self.image = QLabel()
+        self.image.setPixmap(self.imgRed)
+        self.mainlayout.addWidget(self.image)
+
+        self.secured = False
+
+        self.setActive()
+
+    def getStatus(self):
+        return self.secured
+
+    def setActive(self):
+        self.secured = True
+        self.image.setPixmap(self.imgGreen)
+
+    def setInactive(self):
+        self.secured = False
+        self.image.setPixmap(self.imgRed)
+
+    def toggleStatus(self):
+        if self.secured is True:
+            self.secured = False
+            self.image.setPixmap(self.imgRed)
+        else:
+            self.secured = True
+            self.image.setPixmap(self.imgGreen)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    appA = VPNstatusWidget()
+    appA.show()
+    sys.exit(app.exec_())
