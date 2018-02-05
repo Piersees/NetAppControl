74ea0f41b8ae91b4bf6c677c01ac25e64f0ad19e
Ismail AitZaid <aitzaid_i@hotmail.fr>
2018-02-05 16:59:52 +0100

    correction wif_stats

--- Network/Wifi_stat.py
+++ Network/Wifi_stat.py
@@ -11,37 +11,36 @@ Created on Tue Jan 30 14:04:55 2018
 
 import subprocess
 
-#def wifi_info():
-try:
-    available = subprocess.check_output('netsh wlan show network mode=bssid',
+def wifi_info():
+    try:
+        available = subprocess.check_output('netsh wlan show network mode=bssid',
                                         stderr=subprocess.STDOUT,universal_newlines=True,
                                         shell=True)
 
-except subprocess.CalledProcessError:
-    print ("error")
+    except subprocess.CalledProcessError:
+        print ("error")
     #return "Wifi disabled check" #return an error messag
     
-res = []
-res= available.split("\n")
-print (res)
+    res = []
+    res= available.split("\n")
+    print (res)
 
-ssid_dic = {}
-resoc=[]
-actual = None
-for lined in res:
-    if ":" in lined:
-        resoc = lined.split(" : ")
-        for i in range(len(resoc)):
-            resoc[i] = resoc[i].strip()
+    ssid_dic = {}
+    resoc=[]
+    actual = None
+    for lined in res:
+        if ":" in lined:
+            resoc = lined.split(" : ")
+            for i in range(len(resoc)):
+                resoc[i] = resoc[i].strip()
             
-            if "SSID" in resoc[i] and "BSSID" not in resoc[i]:
-                actual = resoc[i]
-                ssid_dic[actual] = {}
+                if "SSID" in resoc[i] and "BSSID" not in resoc[i]:
+                    actual = resoc[i]
+                    ssid_dic[actual] = {}
                 
-            elif actual is not None:
-                ssid_dic[actual][resoc[0]] = resoc[1]
-    #print(ssid_dic)
-    #return ssid_dic
+                elif actual is not None:
+                    ssid_dic[actual][resoc[0]] = resoc[1]
+    return ssid_dic
 
-#if __name__ == "__main__":
- #   wifi_info()
+if __name__ == "__main__":
+    wifi_info()
